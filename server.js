716d0bf084337c1f8a87296f3587e71bc9d7be44
Zeyad <zeyadmsn@aucegypt.edu>
2026-05-26 14:18:58 +0300

    new changes

--- server.js
+++ server.js
@@ -56,7 +56,12 @@ function writeDb(db) {
 }
  
 function todayString() {
-  return new Date().toISOString().slice(0, 10);
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}`;
 }
  
 function getTodaysGame() {
